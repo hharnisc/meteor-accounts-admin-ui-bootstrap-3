5878c0a0391813032de0ed962632acb3de4e31a1
Andy Lash <andy@squidnet.com>
2014-12-01 19:26:40 -0800

    improvement to filter logic thats safer and less restrictive

--- server/publish.js
+++ server/publish.js
@@ -11,15 +11,15 @@ var positiveInteger = Match.Where(function(x) {
   return x >= 0;
 });
 
-var plainString = Match.Where(function(x) {
-  check(x, String);
-  return !!x.match(/^\w*$/);
-});
+// var plainString = Match.Where(function(x) {
+//   check(x, String);
+//   return !!x.match(/^\w*$/);
+// });
 
 Meteor.publish('filteredUsers', function(options) {
   var self = this;
   check(options, Match.ObjectIncluding({
-    filter: plainString,
+    filter: String,
     skip: Match.OneOf(positiveInteger, undefined, null),
     sort: Match.OneOf(Object, undefined, null)
     // sort: Match.OneOf(Match.ObjectIncluding({ direction: Match.OneOf(1, -1), key: String }), null, undefined)
